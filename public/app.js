4ac22f2bffb301987e7851d3153dd492f3408c7c
Abraham <smokiebacon@gmail.com>
2024-07-19 15:32:29 -0700

    change to dynamic dates

--- public/app.js
+++ public/app.js
@@ -2,24 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("/api/mlb-schedule")
     .then((response) => response.json())
     .then((data) => {
-      displayPastGames(data.pastHighScoringGames);
-      displayUpcomingGames(data.futureHomeGames);
+      displayPastGames(data.pastHighScoringGames)
+      displayUpcomingGames(data.futureHomeGames)
     })
-    .catch((error) => console.error("Error:", error));
-});
+    .catch((error) => console.error("Error:", error))
+})
+function todaysDate() {
+  var today = new Date()
+  var dd = today.getDate()
+  var mm = today.getMonth() + 1
+  var yyyy = today.getFullYear()
+  if (dd < 10) {
+    dd = "0" + dd
+  }
+  if (mm < 10) {
+    mm = "0" + mm
+  }
+  today = mm + "/" + dd + "/" + yyyy
+  return today
+}
 
-const todaysDate = new Date();
-const date = "07/18/2024";
+const date = todaysDate()
 //https://github.com/jasonlttl/gameday-api-docs/blob/master/team-information.md
 // https://statsapi.mlb.com/api/v1/schedule?hydrate=team,lineups&sportId=1&startDate=2024-03-01&endDate=2024-07-31&teamId=119
-const dodgersTeamId = 119; //Dodgers Team ID
-const angelsTeamId = 108; // Angels Team ID
-const url = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${dodgersTeamId}`;
-const url2 = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${angelsTeamId}`;
+const dodgersTeamId = 119 //Dodgers Team ID
+const angelsTeamId = 108 // Angels Team ID
+const url = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${dodgersTeamId}`
+const url2 = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${angelsTeamId}`
 
 function displayResult(data, elementId) {
-  const resultDiv = document.getElementById(elementId);
-  resultDiv.innerHTML = "";
+  const resultDiv = document.getElementById(elementId)
+  resultDiv.innerHTML = ""
 
   if (
     data.dates &&
@@ -27,75 +40,75 @@ function displayResult(data, elementId) {
     data.dates[0].games &&
     data.dates[0].games.length > 0
   ) {
-    const game = data.dates[0].games[0];
+    const game = data.dates[0].games[0]
     resultDiv.innerHTML = `
-            <p class="col-lg-8 mx-auto fs-5 text-muted">Game Date: ${game.gameDate}</p>
+            <p class="col-lg-8 mx-auto fs-5 text-muted">Game Date: ${todaysDate()}</p>
             <p>Home Team: ${game.teams.home.team.name}</p>
             <p>Home Team Score: ${game.teams.home.score}</p>
             <p>Home Team Winner: ${game.teams.home.isWinner}</p>
             <p>Away Team: ${game.teams.away.team.name}</p>
             <p>Away Team Score: ${game.teams.away.score}</>
-        `;
+        `
   } else {
-    resultDiv.innerHTML = `No game scheduled for today.`;
+    resultDiv.innerHTML = `No game scheduled for today.`
   }
 }
 
 fetch(url)
   .then((response) => {
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`)
     }
-    return response.json();
+    return response.json()
   })
   .then((data) => {
-    console.log("Dodgers data:", data);
-    displayResult(data, "result");
+    console.log("Dodgers data:", data)
+    displayResult(data, "result")
   })
   .catch((error) => {
-    console.error("Error fetching MLB schedule:", error);
-    document.getElementById("result").innerHTML = `Error: ${error.message}`;
-  });
+    console.error("Error fetching MLB schedule:", error)
+    document.getElementById("result").innerHTML = `Error: ${error.message}`
+  })
 
 fetch(url2)
   .then((response) => {
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`)
     }
-    return response.json();
+    return response.json()
   })
   .then((data) => {
-    console.log("Angels schedule data:", data);
-    displayResult(data, "result2");
+    // console.log("Angels schedule data:", data)
+    displayResult(data, "result2")
   })
   .catch((error) => {
-    console.error("Error fetching MLB schedule:", error);
-    document.getElementById("result").innerHTML = `Error: ${error.message}`;
-  });
+    console.error("Error fetching MLB schedule:", error)
+    document.getElementById("result").innerHTML = `Error: ${error.message}`
+  })
 
 function displayPastGames(games) {
-  const pastDodgerGameWinsTable = document.getElementById("pastGames");
+  const pastDodgerGameWinsTable = document.getElementById("pastGames")
   games.forEach((game) => {
-    const row = document.createElement("tr");
+    const row = document.createElement("tr")
     row.innerHTML = `
               <td>${new Date(game.gameDate).toLocaleString()}</td>
               <td>${game.teams.away.team.name}</td>
               <td>${game.venue.name}</td>
               <td>${game.status.detailedState}</td>
-          `;
-    pastDodgerGameWinsTable.appendChild(row);
-  });
+          `
+    pastDodgerGameWinsTable.appendChild(row)
+  })
 }
 function displayUpcomingGames(games) {
-  const upcomingGamesTable = document.getElementById("upcomingGames");
+  const upcomingGamesTable = document.getElementById("upcomingGames")
   games.forEach((game) => {
-    const row = document.createElement("tr");
+    const row = document.createElement("tr")
     row.innerHTML = `
             <td>${new Date(game.gameDate).toLocaleString()}</td>
             <td>${game.teams.away.team.name}</td>
             <td>${game.venue.name}</td>
             <td>${game.status.detailedState}</td>
-        `;
-    upcomingGamesTable.appendChild(row);
-  });
+        `
+    upcomingGamesTable.appendChild(row)
+  })
 }
--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-import express from "express";
-import path from "path";
-import { todaysDate } from "./date.js";
-import { mlbData } from "./mlb.js";
-const app = express();
-const port = 3000;
+import express from "express"
+import path from "path"
+import { mlbData } from "./mlb.js"
+const app = express()
+const port = 3000
 
 // Serve static files from the 'public' directory
-app.use(express.static("public"));
+app.use(express.static("public"))
 
 app.get("/", async (req, res) => {
-  const date = "07/14/2024";
-  const dodgersTeamId = 119;
-  const angelsTeamId = 108;
-  const url = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${dodgersTeamId}`;
-  const url2 = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${angelsTeamId}`;
+  const date = "07/19/2024"
+  const dodgersTeamId = 119
+  const angelsTeamId = 108
+  const url = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${dodgersTeamId}`
+  const url2 = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&teamId=${angelsTeamId}`
   try {
     const [dodgersResponse, angelsResponse] = await Promise.all([
       fetch(url),
       fetch(url2),
-    ]);
-    const dodgersData = await dodgersResponse.json();
-    const angelsData = await angelsResponse.json();
+    ])
+    const dodgersData = await dodgersResponse.json()
+    const angelsData = await angelsResponse.json()
     const extractGameData = (data) => {
-      const game = data.dates[0]?.games[0];
-      if (!game) return null;
+      const game = data.dates[0]?.games[0]
+      if (!game) return null
 
       return {
         officialDate: game.officialDate,
@@ -33,21 +32,21 @@ app.get("/", async (req, res) => {
         awayTeamName: game.teams.away.team.name,
         awayTeamScore: game.teams.away.score,
         awayTeamWinner: game.teams.away.isWinner,
-      };
-    };
+      }
+    }
 
     const gameData = {
       dodgers: extractGameData(dodgersData),
       angels: extractGameData(angelsData),
-    };
+    }
     if (
       (gameData.homeTeamName =
         "Los Angeles Dodgers" && gameData.dodgers.homeTeamWinner)
     ) {
-      console.log("Free Panda Plate tomorrow!");
+      console.log("Free Panda Plate tomorrow!")
       //email subscribers the next day at 8am
     } else {
-      return "";
+      return ""
     }
 
     if (
@@ -55,56 +54,54 @@ app.get("/", async (req, res) => {
       gameData.angels.homeTeamName &&
       gameData.angels.homeTeamScore >= 7
     ) {
-      console.log("Free Chik sandiwhc");
+      console.log("Free Chik sandiwhc")
       //email subscribers to open to Chik app, getting the free sandwich
     }
 
     res.json({
       dodgers: dodgersData,
       angel: angelsData,
-    });
+    })
   } catch (error) {
-    console.error("Error:", error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while fetching the data" });
+    console.error("Error:", error)
+    res.status(500).json({ error: "An error occurred while fetching the data" })
   }
-});
+})
 
 app.get("/api/mlb-schedule", (req, res) => {
-  const currentDate = new Date();
-  const dodgersId = 119; // Dodgers team ID
+  const currentDate = new Date()
+  const dodgersId = 119 // Dodgers team ID
 
-  const pastHighScoringGames = [];
-  const futureHomeGames = [];
+  const pastHighScoringGames = []
+  const futureHomeGames = []
 
   mlbData.dates.forEach((date) => {
     date.games.forEach((game) => {
-      const gameDate = new Date(game.gameDate);
-      const isDodgersHome = game.teams.home.team.id === dodgersId;
+      const gameDate = new Date(game.gameDate)
+      const isDodgersHome = game.teams.home.team.id === dodgersId
 
       if (gameDate < currentDate) {
         // Past game
         if (isDodgersHome && game.teams.home.score >= 6) {
-          pastHighScoringGames.push({ ...game, isDodgersHome: true });
+          pastHighScoringGames.push({ ...game, isDodgersHome: true })
         }
       } else if (isDodgersHome) {
         // Future home game
-        futureHomeGames.push(game);
+        futureHomeGames.push(game)
       }
-    });
-  });
+    })
+  })
 
   res.json({
     pastHighScoringGames,
     futureHomeGames,
-  });
-});
+  })
+})
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
