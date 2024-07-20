75b7a578b50ac3cb59cb4edc8a757240ff619148
Abraham <smokiebacon@gmail.com>
2024-07-20 00:48:47 -0700

    separate emailing and cache data

---
+++ date.js
@@ -0,0 +1,14 @@
+export function todaysDate() {
+  var today = new Date()
+  var dd = today.getDate() - 1
+  var mm = today.getMonth() + 1
+  var yyyy = today.getFullYear()
+  if (dd < 10) {
+    dd = "0" + dd
+  }
+  if (mm < 10) {
+    mm = "0" + mm
+  }
+  today = mm + "/" + dd + "/" + yyyy
+  return today
+}
