683c675efa4124b2f8b6ea29937440d65f4b47c3
Abraham Hsu <smokiebacon@gmail.com>
2024-10-08 12:28:07 -0700

    update MongoDB pw

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://smokiebacon:ZGrddh7igvvjIUU1@linkful.ztcq2ri.mongodb.net/?retryWrites=true&w=majority&appName=Linkful"
-    )
-    console.log("MongoDB connected successfully")
+      "mongodb+srv://smokiebacon:123456Abe123%23@linkful.ztcq2ri.mongodb.net/?retryWrites=true&w=majority&appName=Linkful"
+    );
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection error:", error)
-    process.exit(1)
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
   }
-}
+};
