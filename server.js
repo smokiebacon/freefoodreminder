d6cb7a64e1ae35d2386c698136ff15dca88827c1
Abraham Hsu <smokiebacon@gmail.com>
2025-03-06 17:20:52 -0800

    add ads.txt file

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(
 );
 
 app.use(express.static("public"));
+app.use("/yelp-randomizer", express.static(path.join(__dirname, "/")));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
